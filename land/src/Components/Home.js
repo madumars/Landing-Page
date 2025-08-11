403f46f5e3a9b74ef9609f0c94f7b3bf5cc82a0f
madumars <mariaeduardam08@gmail.com>
2025-08-11 18:16:42 -0300

    home bar

--- land/src/Components/Home.js
+++ land/src/Components/Home.js
@@ -1,10 +1,23 @@
 import Navbar from "./Navbar";
+import BannerBackground from "../Assets/home-bar.png";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
 
     return(
         <div className="containerHome">
            <Navbar/>
+           <div className="HomeBannerContainer">
+                <div className="HomeBannerImageContainer">
+                    <img src={BannerBackground} alt="imagem de fundo"/>
+                </div>
+                <div className="sectionHomeText">
+                    <h1 className="primaryHeading">Os mais variados sabores de café aqui mesmo</h1>
+                    <p className="primaryText">Conheça nossos sabores e faça seu pedido</p>
+
+                    <button className="secondaryButton">Peça aqui <FaArrowRight/></button>
+                </div>
+           </div>
         </div>
     )
         
